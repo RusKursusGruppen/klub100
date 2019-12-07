9f205a9e36d55b17b73a9e28aff9d9d79125c4c1
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-12-07 16:39:09 +0100

    Updates

---
+++ precache-manifest.4ad6ef613c2e150faec4b44cbb84f5d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663070a319897b47deb86e9eb93d731b",
+    "url": "/klub100/index.html"
+  },
+  {
+    "revision": "32c1d199a4213a61b7ef",
+    "url": "/klub100/static/js/2.aab15e01.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/klub100/static/js/2.aab15e01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df551bd21116a72da85e",
+    "url": "/klub100/static/js/main.3df53cb1.chunk.js"
+  },
+  {
+    "revision": "a288434b0e04d0904bed",
+    "url": "/klub100/static/js/runtime-main.2af598ce.js"
+  }
+]);
